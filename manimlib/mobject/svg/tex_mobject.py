2bfc1e7385b22417ff01378dd06e820915319b4d
ylx120 <i1657027800@outlook.com>
2026-09-19 17:02:13 +0800

    Fix LaTeX rendering on Windows with MiKTeX

--- manimlib/mobject/svg/tex_mobject.py
+++ manimlib/mobject/svg/tex_mobject.py
@@ -27,7 +27,7 @@ def get_tex_mob_scale_factor() -> float:
     # Render a reference "0" and calibrate so that font_size_for_unit_height
     # gives a height of 1 manim unit. Compensates for platform dvisvgm differences.
     font_size_for_unit_height = manim_config.tex.font_size_for_unit_height
-    svg_string = latex_to_svg("0", show_message_during_execution=False)
+    svg_string = latex_to_svg("M", show_message_during_execution=False)
     svg_height = get_svg_content_height(svg_string)
     return 1.0 / (font_size_for_unit_height * svg_height)
 
--- manimlib/utils/tex_file_writing.py
+++ manimlib/utils/tex_file_writing.py
@@ -42,6 +42,7 @@ def get_full_tex(content: str, preamble: str = ""):
         "\n\n".join(
             (
                 "\\documentclass[preview]{standalone}",
+                "\\usepackage{lmodern}",
                 preamble,
                 "\\begin{document}",
                 content,
